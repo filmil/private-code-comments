2d155a6ce1f4c66affcc351ea134418db6af6554
Filip Filmar <fmil@google.com>
2024-01-12 21:27:11 -0800

    fix: more server specific work

--- bin/pcc/main.go
+++ bin/pcc/main.go
@@ -169,9 +169,54 @@ func main() {
 	glog.Infof("JSON-RPC2 id: %v", id)
 
 	Serve(socketFile, db)
+
+	glog.Infof("exiting program")
 }
 
 type Server struct {
+	gotInitialize  bool
+	gotInitialized bool
+	gotShutdown    bool
+	clientInfo     *protocol.ClientInfo
+
+	diagnosticQueue chan struct{}
+	globalCtx       context.Context
+	db              *sql.DB
+	cancel          context.CancelFunc
+}
+
+func NewServer(ctx context.Context, db *sql.DB) *Server {
+	ctx, cancel := context.WithCancel(ctx)
+	s := Server{
+		diagnosticQueue: make(chan struct{}),
+		globalCtx:       ctx,
+		db:              db,
+		cancel:          cancel,
+	}
+
+	go s.DiagnosticsFn()
+
+	return &s
+}
+
+func (s *Server) Shutdown() {
+	s.clientInfo = nil
+	s.gotInitialized = false
+	s.gotInitialize = false
+	s.gotShutdown = true
+}
+
+func (s *Server) DiagnosticsFn() {
+	glog.V(1).Infof("diagnostics up and running")
+	for {
+		select {
+		case <-s.globalCtx.Done():
+			break
+		case _ = <-s.diagnosticQueue:
+			glog.V(1).Infof("queue tick.")
+		}
+	}
+	glog.Infof("global context canceled, exiting")
 }
 
 // GetHandlerFunc returns a stateful function that can be given to jsonrpc2.StreamServer
@@ -184,21 +229,55 @@ func (s *Server) GetHandlerFunc() jsonrpc2.Handler {
 		}()
 
 		switch req.Method() {
+		case protocol.MethodInitialized:
+			if !s.gotInitialize {
+				return fmt.Errorf("got initialized without initialize")
+			}
+			s.gotInitialized = true
+		case protocol.MethodShutdown:
+			s.cancel()
+			s.Shutdown()
+		case protocol.MethodExit:
+			if !s.gotShutdown {
+				glog.Warningf("exiting without shutdown")
+			}
+			return ExitError // this will terminate the serving program.
 		case protocol.MethodInitialize:
 			var p protocol.InitializeParams
 			if err := json.Unmarshal(req.Params(), &p); err != nil {
 				reply(ctx, jsonrpc2.NewError(jsonrpc2.ErrInternal.Code, ""), err)
 				return fmt.Errorf("error during initialize: %v", err)
 			}
+			s.clientInfo = p.ClientInfo
 			glog.V(1).Infof("Request: %v", spew.Sdump(p))
-			// Needs InitializeResponse.
-			var r protocol.InitializeResult
-			r.ServerInfo = &protocol.ServerInfo{
-				Name:    "pcc",
-				Version: "0.0",
+
+			// Result
+			r := protocol.InitializeResult{
+				ServerInfo: &protocol.ServerInfo{
+					Name:    "pcc",
+					Version: "0.0",
+				},
+
+				Capabilities: protocol.ServerCapabilities{
+					//PositionEncoding: "utf-16",
+					TextDocumentSync: &protocol.TextDocumentSyncOptions{
+						OpenClose:         true,
+						Change:            protocol.TextDocumentSyncKindIncremental,
+						WillSave:          true,
+						WillSaveWaitUntil: true,
+						Save: &protocol.SaveOptions{
+							IncludeText: true,
+						},
+					},
+					CodeLensProvider: &protocol.CodeLensOptions{
+						// Have code lens, but no resolve provider.
+						ResolveProvider: false,
+					},
+				},
 			}
 			reply(ctx, r, nil)
-			glog.Infof("Response: %v", spew.Sdump(r))
+			glog.V(1).Infof("Response: %v", spew.Sdump(r)) // This is expensive.
+			s.gotInitialize = true
 		default:
 			reply(ctx, jsonrpc2.ErrMethodNotFound, nil)
 		}
@@ -225,9 +304,12 @@ func (*StdioConn) Write(p []byte) (n int, err error) {
 
 var _ io.ReadWriteCloser = (*StdioConn)(nil)
 
+var ExitError = fmt.Errorf("exiting")
+
 func Serve(f string, db *sql.DB) error {
 	glog.Infof("listening for a connection at: %v", f)
-	s := Server{}
+	ctx := context.Background() // global context
+	s := NewServer(ctx, db)
 	srv := jsonrpc2.HandlerServer(s.GetHandlerFunc())
 
 	if f == defaultSocket {
@@ -235,8 +317,10 @@ func Serve(f string, db *sql.DB) error {
 		jc := jsonrpc2.NewConn(jsonrpc2.NewStream(&StdioConn{}))
 		ctx := context.Background()
 		if err := srv.ServeStream(ctx, jc); err != nil {
-			glog.Infof("error while serving request: %v", err)
-			return err
+			if err != ExitError {
+				glog.Infof("error while serving request: %v", err)
+				return err
+			}
 		}
 	} else {
 		l, err := net.Listen("unix", f)
@@ -257,7 +341,9 @@ func Serve(f string, db *sql.DB) error {
 
 			if err := srv.ServeStream(ctx, jc); err != nil {
 				glog.Infof("error while serving request: %v", err)
-				// Don't exit.
+				if err == ExitError {
+					break
+				}
 			}
 		}
 	}
