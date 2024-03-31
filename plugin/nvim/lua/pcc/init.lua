b7c9319e54ee8cb408f12c75859aff03c0800d6f
Filip Filmar <fmil@google.com>
2024-03-30 21:32:20 -0700

    fix: remove debug code

--- plugin/nvim/lua/pcc/init.lua
+++ plugin/nvim/lua/pcc/init.lua
@@ -395,52 +395,50 @@ disk.
 Add the following configuration in your `init.lua`.
 
 ```lua
--- This setup places the private comments plugin into the directory $HOME/.config/pcc.
 local pcc_dir = os.getenv("HOME") .. "/.config/pcc"
 vim.opt.rtp:prepend(pcc_dir)
 local pcc_plugin = require('pcc')
 
 pcc_plugin.setup_server_with_lsp_config({},
-                {
-                                pcc_binary = os.getenv("HOME") .. "/.local/bin/pcc",
-                                log_dir = os.getenv("HOME") .. '/.local/state/pcc/logs',
-                                db = os.getenv("HOME") .. '/.local/state/pcc/db/db.sqlite',
-                                log_verbosity = 3,
-                                filetypes = {
-                                                "bzl",
-                                                "c",
-                                                "cpp",
-                                                "gn",
-                                                "go",
-                                                "lua",
-                                                "markdown",
-                                                "python",
-                                                "rust",
-                                                "text",
-                                },
-                                autostart = true,
-                }
+    {
+        pcc_binary = os.getenv("HOME") .. "/.local/bin/pcc",
+        log_dir = os.getenv("HOME") .. '/.local/state/pcc/logs',
+        db = os.getenv("HOME") .. '/.local/state/pcc/db/db.sqlite',
+        log_verbosity = 3,
+        filetypes = {
+            "bzl",
+            "c",
+            "cpp",
+            "gn",
+            "go",
+            "lua",
+            "markdown",
+            "python",
+            "rust",
+            "text",
+        },
+        autostart = true,
+    }
 )
 require('lspconfig').pcc.setup {}
 
 vim.api.nvim_create_autocmd("LspAttach", {
-                group = vim.api.nvim_create_augroup("PrivateCodeComments", {}),
-                callback = function(ev)
-                                -- Only create the key mappings when a LSP server attaches
-                                -- to the buffer, and then only for the "pcc" client.
-                                local clients = vim.lsp.get_clients({})
-                                P(ev)
-                                if clients and clients[1] then
-                                                vim.keymap.set(
-                                                                {'n'}, '<leader>cr', pcc_plugin.edit, {
-                                                                                desc = "[C]omment [R]eview"
-                                                                })
-                                                vim.keymap.set(
-                                                                {'n'}, '<leader>cd', pcc_plugin.delete, {
-                                                                                desc = "[C]omment [D]elete"
-                                                                })
-                                end
-                end,
+    group = vim.api.nvim_create_augroup("PrivateCodeComments", {}),
+    callback = function(ev)
+        -- Only create the key mappings when a LSP server attaches
+        -- to the buffer, and then only for the "pcc" client.
+        local clients = vim.lsp.get_clients({ name = "pcc" })
+        if clients and clients[1] then
+            vim.keymap.set(
+                {'n'}, '<leader>cr', pcc_plugin.edit, {
+                    desc = "[C]omment [R]eview"
+            })
+            vim.keymap.set(
+                {'n'}, '<leader>cd', pcc_plugin.delete, {
+                    desc = "[C]omment [D]elete"
+            })
+        end
+    end,
 })
 
 ```
